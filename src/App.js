dc98c38c30c9234d3291877520a4ff7e6c68cb4b
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-08 15:46:45 +0100

    Using the useState Hook

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { useState } from 'react'
+
+function App() {
+  const [name, setName] = useState('mario')
+
+  const handleClick = () => {
+    setName('luigi')
+    console.log(name)
+  }
+
+  return (
+    <div className="App">
+      <h1>My name is {name}</h1>
+      <button onClick={handleClick}>Change name</button>
+    </div>
+  );
+}
+
+export default App;
